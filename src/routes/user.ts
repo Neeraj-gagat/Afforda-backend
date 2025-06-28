5455f7db4b02b623b8994bde8760bf599bb63fcd
Neeraj-gagat <neerajgagat9999@gmail.com>
2025-06-28 19:27:41 +0530

    signup & signin routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,75 @@
 import { Router } from "express";
+import { SigninSchema, SignupSchema } from "../types/types";
+import { prisma } from "../db/db";
+import  Jwt  from "jsonwebtoken";
+import { JWT_PASSWORD } from "../config";
 
 const router = Router();
 
-router.post("/signup", (req,res) => {
+router.post("/signup",async (req,res):Promise <any> => {
+    const body = req.body;
+    const parsedData = SignupSchema.safeParse(body);
     
+    if (!parsedData.success) {
+        console.log(parsedData.error);
+        return res.status(411).json({
+            message:"Incorrect Inputs"
+        })
+    }
+
+    const userexist = await prisma.user.findFirst({
+        where:{
+            email:parsedData.data.email
+        }
+    })
+
+    if (userexist) {
+        return res.status(403).json({
+            message:"User with this email already exist"
+        })
+    }
+
+    await prisma.user.create({
+        data:{
+            email:parsedData.data.email,
+            name:parsedData.data.name,
+            password:parsedData.data.password
+        }
+    })
+
+    return res.json({
+        message:"user Created"
+    })
+})
+
+router.post("/signin", async (req, res):Promise <any> => {
+    const body = req.body;
+    const parsedData = SigninSchema.safeParse(body);
+
+    if (!parsedData.success) {
+        return res.status(422).json({
+            message:"Incorrect inputs"
+        })
+    }
+
+    const userexist = await prisma.user.findFirst({
+        where:{
+            email:parsedData.data.email,
+            password:parsedData.data.password,
+        }
+    })
+
+    if (!userexist) {
+        return res.status(403).json({
+            message:"Wrong Credientials"
+        })
+    }
+
+    const token = Jwt.sign({
+        id:userexist.id
+    }, JWT_PASSWORD)
+
+    res.json({
+        token:token
+    })
 })
