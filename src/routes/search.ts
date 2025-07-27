76c03b7b6e6cef9ed7d11d9ecd070e7d8c038312
Neeraj-gagat <neerajgagat9999@gmail.com>
2025-07-27 21:51:19 +0530

    search-api-hotel

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,9 +1,67 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import {Router } from "express";
+import { SigninSchema } from "../types/types";
+import axios from "axios";
 
 const router = Router();
 
-router.get("/",(req, res ) => {
+router.post("/hotel-result",async(req, res ):Promise <any> => {
+    const body  = req.body;
+    const parsedData = SigninSchema.safeParse(body);
+
+    if (!parsedData.success) {
+        return res.status(400).json({ message: "Invalid request body"})
+    }
+
+    const {
+        checkInDate,
+        checkOutDate,
+        cityId,
+        additional: {
+          language,
+          currency,
+          occupancy: { numberOfAdult, numberOfChildren },
+          maxResult,
+          sortBy,
+        },
+      } = body;
+
+      try {
+        const agodaResponse = await axios.post(
+          "http://affiliateapi7643.agoda.com/affiliateservice/lt_v1", // real Agoda Long Search URL
+          {
+            criteria: {
+              checkInDate,
+              checkOutDate,
+              cityId,
+              additional: {
+                language,
+                currency,
+                occupancy: {
+                  numberOfAdult,
+                  numberOfChildren,
+                },
+                maxResult,
+                sortBy,
+              },
+            },
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${process.env.AGODA_API_KEY}`,
+            },
+          }
+        );
     
+        return res.json(agodaResponse.data);
+      } catch (error: any) {
+        console.error("Agoda API error:", error?.response?.data || error.message);
+        return res.status(500).json({ error: "Agoda Search Failed" });
+      }
+
 })
 
 export const searchRouter = router;
